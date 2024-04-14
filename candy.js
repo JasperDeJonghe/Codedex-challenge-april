f96659ca5c9debc6b455b5bfb40bae0bf7c60c75
Jasper De Jonghe <Jasper.De.Jonghe.07@gmail.com>
2024-04-14 12:05:06 +0200

    Update candy.js

--- candy.js
+++ candy.js
@@ -1,7 +1,7 @@
 const candies = ["Blue", "Orange", "Green", "Yellow", "Red", "Purple"];
 const board = [];
 const rows = 9;
-const colums = 9;
+const columns = 9;
 let score = 0;
 let turns = 0; // Variable to keep track of the number of turns
 
@@ -10,8 +10,8 @@ let scoreQuestions = 0;
 let currTile;
 let otherTile;
 
-let questionsAsked = [false/* ,false,false,false,false,false,false,false,false */]
-let pointsNeeded = [100/* ,350,700,1500,1800,2100,2400,2700,30 */]
+let questionsAsked = [false/* ,false,false,false,false,false,false,false,false */];
+let pointsNeeded = [100/* ,350,700,1500,1800,2100,2400,2700,30 */];
 
 questions = [
     "What type of games do you enjoy the most?",
@@ -23,7 +23,7 @@ questions = [
     "How do you feel about in-game purchases?",
     "How do you react to losing in a game?",
     "How important is the storyline and narrative in a game to you?" */
-]
+];
 choices = [
     ["A. Action/Adventure", "B. Role-playing", "C. Strategy", "D. Simulation", "E. Puzzle", "F. Sports/Racing", "G. Other"],
     /* ["A. Extremely important", "B. Important, but not a deal-breaker", "C. Neutral", "D. Not very important", "E. I don't care about graphics at all"],
@@ -34,7 +34,7 @@ choices = [
     ["A. I'm fine with them if they're purely cosmetic and don't affect gameplay.", "B. I don't mind them as long as they enhance the gaming experience.", "C. I dislike them but tolerate them if the base game is good.", "D. I strongly dislike them and prefer games without any form of in-game purchases."],
     ["A. I get frustrated easily and may rage quit.", "B. I feel disappointed but try again.", "C. Losing doesn't bother me much; it's part of the gaming experience.", "D. I rarely lose, but when I do, I take it as a challenge to improve."],
     ["A. Extremely important; I prefer games with rich, immersive stories.", "B. Important, but gameplay matters more to me.", "C. Neutral; I enjoy games with and without strong narratives.", "D. Not very important; I care more about gameplay mechanics."] */
-]
+];
 
 window.onload = function() {
     startGame();
@@ -46,7 +46,7 @@ window.onload = function() {
         askQuestion();
         updateTasks();
     },100);
-}
+};
 
 function updateTasks() {
     const tasks = document.getElementById("tasks");
@@ -67,19 +67,17 @@ function randomCandy() {
 function startGame() {
     for (let r = 0; r < rows; r++) {
         let row = [];
-        for (let c = 0; c < colums; c++) {
-            // <img id="0-0" src="./images/Red.png">
+        for (let c = 0; c < columns; c++) {
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
-            tile.src = "./images/" + randomCandy() + ".png"; //Change Extension
+            tile.src = "./images/" + randomCandy() + ".png";
 
-            //DRAG FUNCTION
-            tile.addEventListener("dragstart", dragStart); //click on a candy, initialize drag process
-            tile.addEventListener("dragover", dragOver);  //clicking on candy, moving mouse to drag the candy
-            tile.addEventListener("dragenter", dragEnter); //dragging candy onto another candy
-            tile.addEventListener("dragleave", dragLeave); //leave candy over another candy
-            tile.addEventListener("drop", dragDrop); //dropping a candy over another candy
-            tile.addEventListener("dragend", dragEnd); //after drag process completed, we swap candies
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
 
             document.getElementById("board").append(tile);
 
@@ -110,7 +108,6 @@ function dragLeave() {
 function dragDrop() {
     otherTile = this;
 }
-
 
 function dragEnd() {
 
@@ -163,7 +160,7 @@ function crushCandy() {
 function crushFive() {
     // Check rows
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < colums - 4; c++) {
+        for (let c = 0; c < columns - 4; c++) {
             let candies = board[r].slice(c, c + 5); // Get five candies in a row
             let allSame = candies.every(candy => candy.src === candies[0].src); // Check if all candies are the same
             if (allSame && !candies[0].src.includes("blank")) {
@@ -175,7 +172,7 @@ function crushFive() {
     }
 
     // Check columns
-    for (let c = 0; c < colums; c++) {
+    for (let c = 0; c < columns; c++) {
         for (let r = 0; r < rows - 4; r++) {
             let candies = [board[r][c], board[r + 1][c], board[r + 2][c], board[r + 3][c], board[r + 4][c]]; // Get five candies in a column
             let allSame = candies.every(candy => candy.src === candies[0].src); // Check if all candies are the same
@@ -190,7 +187,7 @@ function crushFive() {
 function crushFour() {
     // Check rows
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < colums - 3; c++) {
+        for (let c = 0; c < columns - 3; c++) {
             let candies = board[r].slice(c, c + 4); // Get four candies in a row
             let allSame = candies.every(candy => candy.src === candies[0].src); // Check if all candies are the same
             if (allSame && !candies[0].src.includes("blank")) {
@@ -201,7 +198,7 @@ function crushFour() {
     }
 
     // Check columns
-    for (let c = 0; c < colums; c++) {
+    for (let c = 0; c < columns; c++) {
         for (let r = 0; r < rows - 3; r++) {
             let candies = [board[r][c], board[r + 1][c], board[r + 2][c], board[r + 3][c]]; // Get four candies in a column
             let allSame = candies.every(candy => candy.src === candies[0].src); // Check if all candies are the same
@@ -217,7 +214,7 @@ function crushFour() {
 function crushThree() {
     //check rows
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c<colums-2; c++) {
+        for (let c = 0; c < columns - 2; c++) {
             let candy1 = board[r][c];
             let candy2 = board[r][c+1];
             let candy3 = board[r][c+2];
@@ -231,8 +228,8 @@ function crushThree() {
         }
     }
     //check columns
-    for (let c = 0; c< colums; c++) {
-        for (let r = 0; r<rows-2; r++) {
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 2; r++) {
             let candy1 = board[r][c];
             let candy2 = board[r+1][c];
             let candy3 = board[r+2][c];
@@ -250,7 +247,7 @@ function crushThree() {
 function checkValid() {
     //check rows
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c<colums-2; c++) {
+        for (let c = 0; c < columns - 2; c++) {
             let candy1 = board[r][c];
             let candy2 = board[r][c+1];
             let candy3 = board[r][c+2];
@@ -261,8 +258,8 @@ function checkValid() {
         }
     }
     //check columns
-    for (let c = 0; c< colums; c++) {
-        for (let r = 0; r<rows-2; r++) {
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 2; r++) {
             let candy1 = board[r][c];
             let candy2 = board[r+1][c];
             let candy3 = board[r+2][c];
@@ -277,23 +274,23 @@ function checkValid() {
 }
 
 function slideCandy() {
-    for (let c = 0; c < colums; c++) {
+    for (let c = 0; c < columns; c++) {
         let ind = rows-1;
-        for (let r = rows-1; r>=0; r--) {
+        for (let r = rows-1; r >= 0; r--) {
             if (!board[r][c].src.includes("blank")) {
                 board[ind][c].src = board[r][c].src;
                 ind -= 1;
             }
         }
 
-        for(let r = ind; r>=0; r--) {
+        for(let r = ind; r >= 0; r--) {
             board[r][c].src = "./images/blank.png";
         }
     }
 }
 
 function generateCandy() {
-    for(let c = 0; c < colums; c++) {
+    for(let c = 0; c < columns; c++) {
         if (board[0][c].src.includes("blank")) {
             board[0][c].src = "./images/" + randomCandy() + ".png";
         }
@@ -336,7 +333,6 @@ function displayScoreQuestions() {
 }
 
 
-
 function handleChoice(questionIndex, choiceIndex) {
     // Handle user choice here
     // For example, update score or perform other actions based on choice
@@ -348,6 +344,26 @@ function handleChoice(questionIndex, choiceIndex) {
     questionDisplayed = false;
 
     askQuestion(); // Ask the next question if the score condition is met
+
+    // Assign points based on the choice and question
+    switch (choiceIndex) {
+        case 0: // A
+            scoreQuestions += 3;
+            break;
+        case 1: // B
+            scoreQuestions += 2;
+            break;
+        case 2: // C
+            scoreQuestions += 1;
+            break;
+        default: // D and E
+            // No points assigned for D and E choices
+            break;
+    }
+    console.log("Score Questions:", scoreQuestions);
+
+    // Close the modal after answering
+    document.getElementById("myModal").style.display = "none";
 }
 
 
@@ -375,47 +391,10 @@ function displayQuestionPopup(questionIndex) {
         choiceElement.addEventListener("click", function() {
             // Handle user choice
             handleChoice(questionIndex, index);
-            // Close the modal after answering
-            modal.style.display = "none";
         });
         choicesContainer.appendChild(choiceElement);
     });
 
     // Display modal
     modal.style.display = "block";
-
-    
-}
-
-
-function handleChoice(questionIndex, choiceIndex) {
-    console.log("Question Index:", questionIndex);
-    console.log("Choice Index:", choiceIndex);
-
-    questionDisplayed = false;
-
-    // Assign points based on the choice and question
-    switch (choiceIndex) {
-        case 0: // A
-            scoreQuestions += 3;
-            break;
-        case 1: // B
-            scoreQuestions += 2;
-            break;
-        case 2: // C
-            scoreQuestions += 1;
-            break;
-        default: // D and E
-            // No points assigned for D and E choices
-            break;
-    }
-    console.log("Score Questions:", scoreQuestions);
-
-    const allQuestionsAsked = questionsAsked.every(question => question);
-
-    if (allQuestionsAsked) {
-        // Display modal with total scoreQuestions
-        displayScoreQuestions();
-        return; // Exit the function
-    }
 }
